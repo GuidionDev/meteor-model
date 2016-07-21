163292bcb0c9cd335c7034a18dec6cfe4d92c21e
David Valin <skkunk@gmail.com>
2016-07-21 13:30:15 +0200

    Some ValidationRule testing work

--- tests/unit/validation.specs.js
+++ tests/unit/validation.specs.js
@@ -1,29 +1,41 @@
+"use strict";
 var practicalmeteor_chai_1 = require('meteor/practicalmeteor:chai');
 var meteor_model_1 = require("meteor-model");
 describe("ValidationRule", function () {
+    var validationRule;
+    beforeEach(function () {
+        validationRule = new meteor_model_1.ValidationRule({ firstParamName: 'whatever value', secondParamName: { a: 1001, b: /.*guidion/ } });
+    });
     describe('.constructor()', function () {
-        xit("should assign the value and validator params within the instance", function () {
-            var validationRule = new meteor_model_1.ValidationRule('whatever value', { a: 1001, b: /.*guidion/ });
-            practicalmeteor_chai_1.assert.isDefined(validationRule['value']);
-            practicalmeteor_chai_1.assert.equals(validationRule['value'], 'whatever value');
+        it("should assign the value and validator params within the instance", function () {
             practicalmeteor_chai_1.assert.isDefined(validationRule['params']);
-            practicalmeteor_chai_1.assert.equals(validationRule['params'], { a: 1001, b: /.*guidion/ });
+            practicalmeteor_chai_1.assert.deepEqual(validationRule['params'], { firstParamName: 'whatever value', secondParamName: { a: 1001, b: /.*guidion/ } });
         });
-        describe('.isValid()', function () {
-            it("should validate every condition setted up under 'conditions' attribute", function () {
-            });
+        it("should initialize the _invalidMessage as empty", function () {
+            practicalmeteor_chai_1.assert.equal(validationRule._invalidMessage, "");
         });
-        describe('.addInvalidMessage()', function () {
-            it("should add a message to the invalid messages", function () {
-            });
+    });
+    describe('.isValid()', function () {
+        it("should assign the original and final values within the instance", function () {
+            validationRule.isValid("original value", "new value");
+            practicalmeteor_chai_1.assert.isDefined(validationRule['fromValue']);
+            practicalmeteor_chai_1.assert.equal(validationRule['fromValue'], "original value");
+            practicalmeteor_chai_1.assert.isDefined(validationRule['toValue']);
+            practicalmeteor_chai_1.assert.equal(validationRule['toValue'], "new value");
         });
-        describe('.addCondition()', function () {
-            it("should add a condition to the ValidationRule conditions", function () {
-            });
+        it("should validate every condition setted up under 'conditions' attribute", function () {
         });
-        describe('.removeCondition()', function () {
-            it("should remove a condition from the ValidationRule conditions", function () {
-            });
+    });
+    describe('.addInvalidMessage()', function () {
+        xit("should add a message to the invalid messages", function () {
+        });
+    });
+    describe('.addCondition()', function () {
+        xit("should add a condition to the ValidationRule conditions", function () {
+        });
+    });
+    describe('.removeCondition()', function () {
+        xit("should remove a condition from the ValidationRule conditions", function () {
         });
     });
 });
--- tests/unit/validation.specs.ts
+++ tests/unit/validation.specs.ts
@@ -2,37 +2,51 @@ import { assert } from 'meteor/practicalmeteor:chai';
 import {ValidationRule} from "meteor-model";
 
 describe("ValidationRule", () => {
+  let validationRule;
+
+  beforeEach(() => {
+    validationRule = new ValidationRule({ firstParamName: 'whatever value', secondParamName: { a: 1001, b: /.*guidion/ }});
+  });
+
   describe('.constructor()', () => {
-    xit("should assign the value and validator params within the instance", () => {
-      let validationRule = new ValidationRule('whatever value', { a: 1001, b: /.*guidion/ });
-      assert.isDefined(validationRule['value']);
-      assert.equals(validationRule['value'], 'whatever value');
+    it("should assign the value and validator params within the instance", () => {
       assert.isDefined(validationRule['params']);
-      assert.equals(validationRule['params'], { a: 1001, b: /.*guidion/ });
+      assert.deepEqual(validationRule['params'], { firstParamName: 'whatever value', secondParamName: { a: 1001, b: /.*guidion/ }});
     });
 
-    describe('.isValid()', () => {
-      it("should validate every condition setted up under 'conditions' attribute", () => {
-
-      });
+    it("should initialize the _invalidMessage as empty", () => {
+      assert.equal(validationRule._invalidMessage, "");
     });
+  });
 
-    describe('.addInvalidMessage()', () => {
-      it("should add a message to the invalid messages", () => {
-
-      });
+  describe('.isValid()', () => {
+    it("should assign the original and final values within the instance", () => {
+      validationRule.isValid("original value", "new value");
+      assert.isDefined(validationRule['fromValue']);
+      assert.equal(validationRule['fromValue'], "original value");
+      assert.isDefined(validationRule['toValue']);
+      assert.equal(validationRule['toValue'], "new value");
     });
+    it("should validate every condition setted up under 'conditions' attribute", () => {
 
-    describe('.addCondition()', () => {
-      it("should add a condition to the ValidationRule conditions", () => {
-
-      });
     });
+  });
 
-    describe('.removeCondition()', () => {
-      it("should remove a condition from the ValidationRule conditions", () => {
+  describe('.addInvalidMessage()', () => {
+    xit("should add a message to the invalid messages", () => {
 
-      });
+    });
+  });
+
+  describe('.addCondition()', () => {
+    xit("should add a condition to the ValidationRule conditions", () => {
+
+    });
+  });
+
+  describe('.removeCondition()', () => {
+    xit("should remove a condition from the ValidationRule conditions", () => {
+
     });
   });
 });
