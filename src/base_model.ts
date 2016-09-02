7bd0cab75eb19390b16bde53af79fe39a8be50c3
Rene <rslump@guidion.net>
2016-09-02 13:51:54 +0200

    feat(baseModel): Made a separation between meteor/mongo functionality and validation functionality. The latter is handled in a base model which is used by meteor model, but can also be extended from separatly in case you dont want to persist the object in its own collection.

---
+++ src/base_model.ts
@@ -0,0 +1,187 @@
+import {ValidationRule} from './validation';
+
+/**
+ * Implements a base class on top of we implement our models.
+ * It works in the server and in the browser.
+ */
+export class BaseModel {
+  protected _attrs: any
+  protected _prevAttrs: Object
+  private _errors: Object
+  private validationRules: Object
+
+  constructor(initialAttributes:Object = {}) {
+
+    this._attrs = {};
+    this._prevAttrs = {};
+    this._errors = {};
+
+    // Extend with defaults first
+    _.extend(this._attrs, this.defaults());
+
+    // Merge with initialization attributes too
+    const initialAttributesKeys = Object.keys(initialAttributes);
+    for (let i = 0; i < initialAttributesKeys.length; i++) {
+      this._attrs[initialAttributesKeys[i]] = initialAttributes[initialAttributesKeys[i]];
+    }
+
+    // On initialization, the prev attributes are the initial attributes
+    Object.assign(this._prevAttrs, this._attrs);
+  }
+
+  get errors() : Object { return this._errors; }
+  public getAttrErrors(attributeName:string) : Object {
+    return (this._errors[attributeName] ? this._errors[attributeName] : []);
+  }
+
+  // Callbacks to run before and after the record validation
+  private beforeValidation() {}
+  private afterValidation() {}
+
+  // NOTE: Implement it when extending
+  /**
+   * Sets the default values for the attributes when a new instance is created
+   */
+  public defaults() : Object {
+    return {};
+    // Sample:
+    //
+    // return {
+    //   'name' : 'Chuck Norris',
+    //   'active' : true
+    // }
+  }
+  
+  protected addUniqueValueToArray(collectionName:string, attrValue:any) : boolean {
+    if (this._attrs[collectionName].indexOf(attrValue) < 0) {
+      this._attrs[collectionName].push(attrValue);
+      return true;
+      }
+    return false;
+  }
+
+  protected removeValueFromArray(collectionName:string, attrValue) : boolean {
+    let index = this._attrs[collectionName].indexOf(attrValue);
+    if ( index >= 0) {
+        this._attrs[collectionName].splice(index, 1);
+        return true;
+    }
+    return false;
+  }
+
+  /**
+   * Adds a validation error on a specific attribute
+   */
+  private addValidationError(attributeName:string, errorMessage:string) : void {
+    if (!this._errors[attributeName]) {
+      this._errors[attributeName] = [];
+    }
+    this._errors[attributeName].push(errorMessage);
+  }
+
+  /**
+   * Validates the model according to its ValidationRules
+   */
+  public validate() : boolean {
+    console.log(' + validating the document');
+    this.beforeValidation();
+
+    // Reset errors
+    this._errors = {};
+
+    let attrNames = Object.keys(this._attrs),
+        matchAllValidations = true;
+
+    // Validate every ValidationRule in every attribute
+    for (let i = 0; i < attrNames.length; i++) {
+      if (this.validationRules[attrNames[i]]) {
+        if (!this.validateAttr(attrNames[i])) {
+          matchAllValidations = false;
+        }
+      }
+    }
+
+    // Validate all custom ValidationRules
+    if (this.validationRules['_base'] && this.validationRules['_base'].length > 0) {
+      for (let i = 0; i < this.validationRules['_base'].length; i++) {
+        if (!this.validationRules['_base'][i]()) {
+          matchAllValidations = false;
+        }
+      }
+    }
+
+    this.afterValidation();
+    return matchAllValidations;
+  }
+
+  /**
+   * Checks if the model is a valid model according to its ValidationRules
+   */
+  public isValid() : boolean {
+    let invalidAttrs = Object.keys(this['_errors']);
+    return (invalidAttrs.length === 0 ? true : false);
+  }
+
+  /**
+   * Validates a model attribute according to its ValidationRules
+   */
+  public validateAttr(attributeName:string) : boolean {
+    let matchAllValidations = true,
+        validationRule;
+    for (let i = 0; i < this.validationRules[attributeName].length; i++) {
+      validationRule = this.validationRules[attributeName][i];
+
+      // Check if entity is valid and collect all validation errors if any
+      if (!validationRule.isValid(this._prevAttrs[attributeName], this._attrs[attributeName])) {
+        // Add the validator message to the MeteorModel
+        this.addValidationError(attributeName, validationRule._invalidMessage);
+        matchAllValidations = false;
+      }
+    }
+
+    return matchAllValidations;
+  }
+
+  /**
+   * Checks whether an attribute value is valid
+   */
+  public isValidAttr(attributeName:string) : boolean {
+    return (this.getAttrErrors(attributeName).length === 0 ? true : false);
+  }
+
+  /**
+   * Checks whether the model has attributes changed since the last sync
+   */
+  public hasChanged() : boolean {
+    let attrName, prevAttrName,
+        attrNames = Object.keys(this._attrs),
+        prevAttrNames = Object.keys(this._prevAttrs);
+
+    if (attrNames.length !== prevAttrNames.length) {
+      return true;
+    }
+
+    for (prevAttrName of prevAttrNames) {
+      if (this._prevAttrs[prevAttrName] !== this._attrs[prevAttrName]) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  /**
+   * Checks whether a model attribute has changed since the last sync
+   */
+  public hasAttrChanged(attrName:string) : boolean {
+    return (this._attrs[attrName] !== this._prevAttrs[attrName]);
+  }
+
+  /**
+   * Returns the JSON expected by the Meteor endpoints.
+   * Override it in your extended classes when you need to process a different JSON structure
+   */
+  public toOriginJSON() : Object {
+    return this['_attrs'];
+  }
+}
--- src/meteor_model.ts
+++ src/meteor_model.ts
@@ -1,41 +1,16 @@
 import {ValidationRule} from './validation';
+import {BaseModel} from './base_model';
 
 /**
  * Implements a base class on top of we implement our models.
  * It works in the server and in the browser.
  */
-export class MeteorModel {
+export class MeteorModel extends BaseModel {
   private transport = "Meteor";
-  protected _attrs: any
-  protected _prevAttrs: Object
-  private _errors: Object
-  private validationRules: Object
   public static COLLECTION: any
   public static COLLECTION_NAME = 'default'
 
-  constructor(initialAttributes:Object = {}) {
-
-    this._attrs = {};
-    this._prevAttrs = {};
-    this._errors = {};
-
-    // Extend with defaults first
-    _.extend(this._attrs, this.defaults());
-
-    // Merge with initialization attributes too
-    const initialAttributesKeys = Object.keys(initialAttributes);
-    for (let i = 0; i < initialAttributesKeys.length; i++) {
-      this._attrs[initialAttributesKeys[i]] = initialAttributes[initialAttributesKeys[i]];
-    }
-
-    // On initialization, the prev attributes are the initial attributes
-    Object.assign(this._prevAttrs, this._attrs);
-  }
-
-  get errors() : Object { return this._errors; }
-  public getAttrErrors(attributeName:string) : Object {
-    return (this._errors[attributeName] ? this._errors[attributeName] : []);
-  }
+  constructor(initialAttributes:Object) { super(initialAttributes); }
 
   get id() : any {
     return this._attrs._id;
@@ -46,10 +21,6 @@ export class MeteorModel {
    */
   private beforeCreate() { }
   private afterCreate() { }
-
-  // Callbacks to run before and after the record validation
-  private beforeValidation() {}
-  private afterValidation() {}
 
   // Callbacks to run before and after saving the record
   private beforeSave() { }
@@ -63,151 +34,11 @@ export class MeteorModel {
   private beforeDestroy() { }
   private afterDestroy() { }
 
-  // NOTE: Implement it when extending
-  /**
-   * Sets the default values for the attributes when a new instance is created
-   */
-  public defaults() : Object {
-    return {};
-    // Sample:
-    //
-    // return {
-    //   'name' : 'Chuck Norris',
-    //   'active' : true
-    // }
-  }
-  
-  protected addUniqueValueToArray(collectionName:string, attrValue:any) : boolean {
-    if (this._attrs[collectionName].indexOf(attrValue) < 0) {
-      this._attrs[collectionName].push(attrValue);
-      return true;
-      }
-    return false;
-  }
-
-  protected removeValueFromArray(collectionName:string, attrValue) : boolean {
-    let index = this._attrs[collectionName].indexOf(attrValue);
-    if ( index >= 0) {
-        this._attrs[collectionName].splice(index, 1);
-        return true;
-    }
-    return false;
-  }
-
-
   /**
    * Checks whether the MeteorModel instance is a new record
    */
   public isNew() : boolean {
     return (!this.id);
-  }
-
-  /**
-   * Adds a validation error on a specific attribute
-   */
-  private addValidationError(attributeName:string, errorMessage:string) : void {
-    if (!this._errors[attributeName]) {
-      this._errors[attributeName] = [];
-    }
-    this._errors[attributeName].push(errorMessage);
-  }
-
-  /**
-   * Validates the model according to its ValidationRules
-   */
-  public validate() : boolean {
-    console.log(' + validating the document');
-    this.beforeValidation();
-
-    // Reset errors
-    this._errors = {};
-
-    let attrNames = Object.keys(this._attrs),
-        matchAllValidations = true;
-
-    // Validate every ValidationRule in every attribute
-    for (let i = 0; i < attrNames.length; i++) {
-      if (this.validationRules[attrNames[i]]) {
-        if (!this.validateAttr(attrNames[i])) {
-          matchAllValidations = false;
-        }
-      }
-    }
-
-    // Validate all custom ValidationRules
-    if (this.validationRules['_base'] && this.validationRules['_base'].length > 0) {
-      for (let i = 0; i < this.validationRules['_base'].length; i++) {
-        if (!this.validationRules['_base'][i]()) {
-          matchAllValidations = false;
-        }
-      }
-    }
-
-    this.afterValidation();
-    return matchAllValidations;
-  }
-
-  /**
-   * Checks if the model is a valid model according to its ValidationRules
-   */
-  public isValid() : boolean {
-    let invalidAttrs = Object.keys(this['_errors']);
-    return (invalidAttrs.length === 0 ? true : false);
-  }
-
-  /**
-   * Validates a model attribute according to its ValidationRules
-   */
-  public validateAttr(attributeName:string) : boolean {
-    let matchAllValidations = true,
-        validationRule;
-    for (let i = 0; i < this.validationRules[attributeName].length; i++) {
-      validationRule = this.validationRules[attributeName][i];
-
-      // Check if entity is valid and collect all validation errors if any
-      if (!validationRule.isValid(this._prevAttrs[attributeName], this._attrs[attributeName])) {
-        // Add the validator message to the MeteorModel
-        this.addValidationError(attributeName, validationRule._invalidMessage);
-        matchAllValidations = false;
-      }
-    }
-
-    return matchAllValidations;
-  }
-
-  /**
-   * Checks whether an attribute value is valid
-   */
-  public isValidAttr(attributeName:string) : boolean {
-    return (this.getAttrErrors(attributeName).length === 0 ? true : false);
-  }
-
-  /**
-   * Checks whether the model has attributes changed since the last sync
-   */
-  public hasChanged() : boolean {
-    let attrName, prevAttrName,
-        attrNames = Object.keys(this._attrs),
-        prevAttrNames = Object.keys(this._prevAttrs);
-
-    if (attrNames.length !== prevAttrNames.length) {
-      return true;
-    }
-
-    for (prevAttrName of prevAttrNames) {
-      if (this._prevAttrs[prevAttrName] !== this._attrs[prevAttrName]) {
-        return true;
-      }
-    }
-
-    return false;
-  }
-
-  /**
-   * Checks whether a model attribute has changed since the last sync
-   */
-  public hasAttrChanged(attrName:string) : boolean {
-    return (this._attrs[attrName] !== this._prevAttrs[attrName]);
   }
 
   /**
@@ -218,7 +49,8 @@ export class MeteorModel {
     if (Meteor.isServer) {
       console.log(' + Running .save() in the backend', this._attrs);
       if (this.isNew()) {
-        return this.constructor['COLLECTION'].insert(this._attrs);
+        const id = this.constructor['COLLECTION'].insert(this._attrs);
+        return this._attrs._id = id;
       } else {
         return this.constructor['COLLECTION'].update({_id: this.id}, this._attrs);
       }
